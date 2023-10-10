712553b1de2a8c11ea923840daf7f8f14af3c7d5
Reece Hunter <heecerunter@gmail.com>
2023-10-10 12:41:57 -0600

    Added https to api url

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,6 +1,6 @@
 console.log(process.env.NODE_ENV)
 const config = {
-  api_url: process.env.NODE_ENV === "development" ? "http://localhost:5050" : "http://100.24.29.2:5050",
+  api_url: process.env.NODE_ENV === "development" ? "http://localhost:5050" : "https://100.24.29.2:5050",
   default_user_image:
     "https://static.vecteezy.com/system/resources/thumbnails/002/534/006/small/social-media-chatting-online-blank-profile-picture-head-and-body-icon-people-standing-icon-grey-background-free-vector.jpg",
   default_plugin_image: "https://cdn.modrinth.com/data/Lu3KuzdV/b2c4b7b0033ab09cc166f2848003ef3a02c70a83.png",
