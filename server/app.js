0100be35a0f23bd3d049cac9b7214fff03518fbe
Reece Hunter <heecerunter@gmail.com>
2023-10-19 16:54:54 -0600

    Added https

--- server/app.js
+++ server/app.js
@@ -1,13 +1,29 @@
 const express = require("express");
-const app = express();
+const https = require("https");
+const http = require("http");
+const fs = require("fs");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const config = require("./config/config");
-const port = config.port;
+let port = config.port;
+
+const app = express();
 
 app.use(express.json(), express.urlencoded({ extended: true }), cookieParser(), cors({ origin: "http://localhost:3000", credentials: true }));
 
 require("./routes/users.route")(app);
 require("./routes/plugins.route")(app);
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+let server;
+try {
+    const options = {
+        key: fs.readFileSync("./certs/key.pem"),
+        cert: fs.readFileSync("./certs/cert.pem"),
+    };
+    server =  https.createServer(options, app);
+    port = 443;
+} catch(err) {
+    server = http.createServer(app);
+}
+
+server.listen(port, () => console.log(`Listening on port ${port}`));
